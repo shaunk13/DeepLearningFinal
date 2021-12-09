495e8d6b15bf69a7c54a3517f01176320f5c610f
shaunk13 <67980215+shaunk13@users.noreply.github.com>
2021-12-08 22:26:57 -0500

    Delete helpers.py

--- helpers.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-def checkerboard(height, width, r=False):
-    """
-    Takes in the dimensions of the checkerboard mask to be produced.
-    Will output a 2D tf tensor containing a checkerboard pattern of 1s and 0s.
-    """
-    checkerboard = [[(i + j) % 2 for j in range(width)] for i in range(height)]
-    return tf.convert_to_tensor(checkerboard)
